0c682f5fabee1ca54610842dab2a985ab13b8028
Frederik Omlor <freddy.omlor@korion.de>
2024-04-10 13:15:57 +0200

    Removed unnecessary usings

--- Assets/ScotlandYard/Scripts/Network/GameSetupSystem.cs
+++ Assets/ScotlandYard/Scripts/Network/GameSetupSystem.cs
@@ -1,9 +1,6 @@
-using Newtonsoft.Json.Bson;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using UnityEngine;
 
 public class GameSetupSystem : NetworkSystem<GameSetupEvents, GameSetupSystem>
