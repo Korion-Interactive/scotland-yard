9066accb3365b3e63a140de97bc57a320172016e
Frederik Omlor <freddy.omlor@korion.de>
2024-04-15 11:05:24 +0200

    Fixed again after merge: start button not working on pc (controller)

--- Assets/ScotlandYard/Scripts/GUI/ClickByKey.cs
+++ Assets/ScotlandYard/Scripts/GUI/ClickByKey.cs
@@ -7,6 +7,7 @@ public class ClickByKey : MonoBehaviour
     public KeyCode Key = KeyCode.Escape; // Escape is "Back" on Android
 
 
+#if !UNITY_EDITOR && !UNITY_PS4 && !UNITY_PS5 && !UNITY_STANDALONE
     void Update()
     {
         if (Input.GetKeyUp(Key) && ! IsBlockedGlobally)
@@ -14,5 +15,6 @@ public class ClickByKey : MonoBehaviour
             SendMessage("OnClick");
         }
     }
+#endif
 
 }
